f8beb8610c22b9d10520830f3d1a89723bea18f2
mariolyr30 <91512462+mariolyr30@users.noreply.github.com>
2021-12-01 22:28:38 -0700

    Update profesor.php

--- profesor.php
+++ profesor.php
@@ -3,34 +3,34 @@
 include("con_db.php");
 
 if (isset($_POST['profesor'])) {
-    if (strlen($_POST['dniprofesor']) >= 1 && 
-	strlen($_POST['nombre(s)']) >= 1 && 
-	strlen($_POST['apellidos']) >= 1 &&
-	strlen($_POST['telefono']) >= 1 &&
-	strlen($_POST['domicilio']) >= 1 && 
-	strlen($_POST['fecha_de_nacimiento']) >= 1 &&
-	strlen($_POST['sexo']) >= 1 &&
-	strlen($_POST['materia']) >= 1 && 
-	strlen($_POST['email']) >= 1 &&
-	strlen($_POST['grupos']) >= 1 &&
-	strlen($_POST['salon']) >= 1 && 
-	strlen($_POST['turno']) >= 1) {
+    if (strlen($_POST['DNI']) >= 1 && 
+	strlen($_POST['Nombre(s)']) >= 1 && 
+	strlen($_POST['Apellidos']) >= 1 &&
+	strlen($_POST['Telefono']) >= 1 &&
+	strlen($_POST['Domicilio']) >= 1 && 
+	strlen($_POST['Fecha_de_nacimiento']) >= 1 &&
+	strlen($_POST['Sexo']) >= 1 &&
+	strlen($_POST['Materia']) >= 1 && 
+	strlen($_POST['Email']) >= 1 &&
+	strlen($_POST['Grupos']) >= 1 &&
+	strlen($_POST['Salon']) >= 1 && 
+	strlen($_POST['Turno']) >= 1) {
 
-	    $dniprofesor = trim($_POST['dniprofesor']);
-	    $nombre = trim($_POST['nombre(s)']);
-	    $apellidos = trim($_POST['apellidos']);
-		$telefono = trim($_POST['telefono']);
-		$domicilio = trim($_POST['domicilio']);
-		$fecha_de_nacimiento = trim($_POST['fecha_de_nacimiento']);
-	    $sexo = trim($_POST['sexo']);
-		$materia = trim($_POST['materia']);
-		$email = trim($_POST['email']);
-		$grupos = trim($_POST['grupos']);
-	    $salon = trim($_POST['salon']);
-		$turno = trim($_POST['turno']);
+	    $DNIprofesor = trim($_POST['DNI']);
+	    $Nombre = trim($_POST['nombre(s)']);
+	    $Apellidos = trim($_POST['apellidos']);
+		$Telefono = trim($_POST['telefono']);
+		$Domicilio = trim($_POST['domicilio']);
+		$Fecha_de_nacimiento = trim($_POST['fecha_de_nacimiento']);
+	    $Sexo = trim($_POST['sexo']);
+		$Materia = trim($_POST['materia']);
+		$Email = trim($_POST['email']);
+		$Grupos = trim($_POST['grupos']);
+	    $Salon = trim($_POST['salon']);
+		$Turno = trim($_POST['turno']);
 
 	    $consulta = "INSERT INTO profesor (dniprofesor, nombre(s), apellidos, telefono, domicilio, fecha_de_nacimiento, sexo, materia, email grupos, salon, turno) 
-		VALUES ('$dniprofesor','$nombre','$apellidos','$telefono','$domicilio','$fecha_de_nacimiento','$sexo','$materia','$email','$grupos','$salon','$turno)";
+		VALUES ('$DNIprofesor','$Nombre','$Apellidos','$Telefono','$Domicilio','$Fecha_de_nacimiento','$Sexo','$Materia','$Email','$Grupos','$Salon','$Turno)";
 
 	    $resultado = mysqli_query($conex,$consulta);
 	    if ($resultado) {
